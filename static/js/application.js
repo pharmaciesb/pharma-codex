58dbf5d08a89a46da1fce966aba34d4f5249fe89
ZinkStudio <zink13013@gmail.com>
2025-10-07 13:40:46 +0200

    Delete static/js directory

--- static/js/application.js
+++
@@ -1,4 +0,0 @@
-// Détection auto du préfixe GitHub Pages
-window.BASE_URL = window.location.pathname.includes("/pharma-codex/") ? "/pharma-codex" : "";
-import { loadComponentsFromConfig } from '/static/js/loader.js';
-await loadComponentsFromConfig();
